49ad639ef74b8574b30d83fd7f442cf11786859b
vilin <616954589@qq.com>
2021-04-14 16:01:27 +0800

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# vue-xpl-tree
+参考[vue-giant-tree](https://github.com/tower1229/Vue-Giant-Tree)
+由于实际的需求新增了一些功能，基本的使用方法可以参考[vue-giant-tree](https://github.com/tower1229/Vue-Giant-Tree)和[ztree](https://github.com/zTree/zTree_v3)
+
+## 安装
+
+```
+npm i vue-xpl-ztree --save
+```
+
+## 功能
+- 树的模糊搜索 fuzzySearch
+## 其他
+- 提取css，生成独立的样式文件
---
+++ src/components/ztree.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="ztree" :id="ztreeId"></div>
+</template>
+
+<script>
+import * as $ from 'jquery'
+
+if (!window.jQuery) {
+  window.jQuery = $
+}
+require('@ztree/ztree_v3/js/jquery.ztree.all')
+require('@ztree/ztree_v3/js/jquery.ztree.exhide')
+require('@/assets/ztree/fuzzysearch.module')
+
+export default {
+  props: {
+    setting: {
+      type: Object,
+      require: false,
+      default: function () {
+        return {}
+      }
+    },
+    nodes: {
+      type: Array,
+      require: true,
+      default: function () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      ztreeId: 'ztree_' + parseInt(Math.random() * 1e10),
+      ztreeObj: null,
+      list: [],
+      ztreeSetting: {
+        view: {
+          showIcon: false         // default to hide icon
+        },
+        callback: {
+          onAsyncError: (...arg) => {
+            this.$emit('onAsyncError', ...arg)
+          },
+          onAsyncSuccess: (...arg) => {
+            this.$emit('onAsyncSuccess', ...arg)
+          },
+          onCheck: (...arg) => {
+            this.$emit('onCheck', ...arg)
+          },
+          onClick: (...arg) => {
+            this.$emit('onClick', ...arg)
+          },
+          onCollapse: (...arg) => {
+            this.$emit('onCollapse', ...arg)
+          },
+          onDblClick: (...arg) => {
+            this.$emit('onDblClick', ...arg)
+          },
+          onDrag: (...arg) => {
+            this.$emit('onDrag', ...arg)
+          },
+          onDragMove: (...arg) => {
+            this.$emit('onDragMove', ...arg)
+          },
+          onDrop: (...arg) => {
+            this.$emit('onDrop', ...arg)
+          },
+          onExpand: (...arg) => {
+            this.$emit('onExpand', ...arg)
+          },
+          onMouseDown: (...arg) => {
+            this.$emit('onMouseDown', ...arg)
+          },
+          onMouseUp: (...arg) => {
+            this.$emit('onMouseUp', ...arg)
+          },
+          onRemove: (...arg) => {
+            this.$emit('onRemove', ...arg)
+          },
+          onRename: (...arg) => {
+            this.$emit('onRename', ...arg)
+          },
+          onRightClick: (...arg) => {
+            this.$emit('onRightClick', ...arg)
+          }
+        }
+      }
+    }
+  },
+  watch: {
+    nodes: {
+      handler: function (nodes) {
+        this.list = nodes
+
+        // update tree
+        if (this.ztreeObj) {
+          this.ztreeObj.destroy()
+        }
+        this.$nextTick(() => {
+          this.ztreeObj = $.fn.zTree.init($('#' + this.ztreeId), Object.assign({}, this.ztreeSetting, this.setting), this.list)
+          this.$emit('onCreated', this.ztreeObj)
+        })
+      },
+      deep: true,
+      immediate: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* beauty ztree! */
+@import "ztree.css";
+</style>
